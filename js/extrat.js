a48685d6e2428a1a0b197956c4533cf2a6448499
jpiitulainen <jpiitulainen2001@gmail.com>
2026-04-26 23:55:06 +0300

    Lähteet lisätty

--- js/extrat.js
+++ js/extrat.js
@@ -10,3 +10,14 @@ function updateMoves() {
   moves++;
   document.getElementById("moves").textContent = moves;
 }
+
+/*
+Lähteet:
+W3Schools - JavaScript Timing Events
+https://www.w3schools.com/js/js_timing.asp
+
+JavaScript.info - setTimeout and setInterval
+https://javascript.info/settimeout-setinterval
+
+OpenAI ChatGPT (GPT-5) Tekoälyä käytetty koodin korjaamiseen ja selittämiseen.
+*/
