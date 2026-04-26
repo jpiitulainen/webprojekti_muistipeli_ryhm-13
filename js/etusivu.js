428ef738c99abfac4cca0f85173f7a97782f8f9c
jpiitulainen <jpiitulainen2001@gmail.com>
2026-04-27 00:14:44 +0300

    Lähteet lisätty

--- js/etusivu.js
+++ js/etusivu.js
@@ -13,3 +13,20 @@ setInterval(moveFish, 2000);
 window.onload = () => {
   document.body.style.opacity = "1";
 };
+
+/*
+Lähteet:
+
+W3Schools - querySelector()
+https://www.w3schools.com/jsref/met_document_queryselector.asp
+
+W3Schools - Math.random()
+https://www.w3schools.com/jsref/jsref_random.asp
+
+JavaScript.info - Functions
+https://javascript.info/function-basics
+
+JavaScript.info - setTimeout and setInterval
+https://javascript.info/settimeout-setinterval
+
+OpenAI - ChatGPT (GPT-5) Tekoälyä käytetty korjaamaan koodia ja selittämään sitä.
